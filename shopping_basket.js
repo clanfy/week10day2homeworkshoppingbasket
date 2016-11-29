e0c796e883a6c1712bb9052efc04cb9491998015
Clacson <cookiellacson@gmail.com>
2016-11-29 16:32:09 +0000

    shopping basket object started

--- shopping_basket.js
+++ shopping_basket.js
@@ -1,5 +1,16 @@
+
+
 var shoppingBasket = {
   basket: []
+  returnTotal: function(){
+    var i = 0;
+    var total = 0;
+
+    for (i, i < arguments.length, i ++){
+      total += arguments[i].price
+    }
+    return total;
+  }
 }
 
 module.exports = shoppingBasket;
