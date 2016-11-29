191332cec721f7c29df5ea6af113ddcee7d6f3a4
Clacson <cookiellacson@gmail.com>
2016-11-29 17:20:09 +0000

    9th test passed, eggs have price

--- specs/eggs_spec.js
+++ specs/eggs_spec.js
@@ -5,5 +5,10 @@ describe("Eggs", function(){
 
   it ("should have brand", function(){
     assert.equal("Chickening Out", eggs.brand);
-  })
+  });
+
+  it("should have price", function(){
+    assert.equal(2.00, eggs.price);
+  });
+
 })
